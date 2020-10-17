5ea1d9cf5564df90755361f33ebafb9946e48999
Hossam <hossamhsn74@gmail.com>
2020-10-17 04:57:59 +0200

    setup basic API

--- bookstore/api/views.py
+++ bookstore/api/views.py
@@ -1,11 +1,62 @@
-# from rest_framework.decorators import api_view, permission_classes
-# from rest_framework.permissions import IsAuthenticated
-# from django.http import JsonResponse
-# from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from . import serializers
+from ..models import Book
+from rest_framework import generics, status
+from rest_framework.response import Response
 
+# example of using function based views
 # @api_view(["GET"])
 # @csrf_exempt
-# @permission_classes([IsAuthenticated])  
+# @permission_classes([IsAuthenticated])
 # def welcome(request):
 #     content = {"message": "Welcome to the BookStore!"}
 #     return JsonResponse(content)
+
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = serializers.BookSerializer
+
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = serializers.BookSerializer
+
+    def create(self, request, *args, **kwargs):
+        super(BookCreateView, self).create(request, args, kwargs)
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully added to books",
+                    "result": request.data}
+        return Response(response)
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = serializers.BookSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        super(BookDetailView, self).retrieve(request, args, kwargs)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        data = serializer.data
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully retrieved",
+                    "result": data}
+        return Response(response)
+
+    def patch(self, request, *args, **kwargs):
+        super(BookDetailView, self).patch(request, args, kwargs)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        data = serializer.data
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully updated",
+                    "result": data}
+        return Response(response)
+
+    def delete(self, request, *args, **kwargs):
+        super(BookDetailView, self).delete(request, args, kwargs)
+        response = {"status_code": status.HTTP_200_OK,
+                    "message": "Successfully deleted"}
+        return Response(response)
--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,6 +1,12 @@
-from django.contrib import admin
 from django.urls import path
+from .api import views
+
+# /books >> get, post
+# /books/id >>get,put,delete,patch
 
 urlpatterns = [
-   
+    path('', views.BookListView.as_view(), name='booklist'),
+    path('create/', views.BookCreateView.as_view(), name='createbook'),
+    path('<int:pk>/', views.BookDetailView.as_view(), name='updatebook'),
+
 ]
