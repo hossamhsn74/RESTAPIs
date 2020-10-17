0859244a618e92df5adf4ec58faa83dcee8d4174
Hossam <hossamhsn74@gmail.com>
2020-10-17 03:26:43 +0200

    inital commit

---
+++ bookstore/api/views.py
@@ -0,0 +1,11 @@
+# from rest_framework.decorators import api_view, permission_classes
+# from rest_framework.permissions import IsAuthenticated
+# from django.http import JsonResponse
+# from django.views.decorators.csrf import csrf_exempt
+
+# @api_view(["GET"])
+# @csrf_exempt
+# @permission_classes([IsAuthenticated])  
+# def welcome(request):
+#     content = {"message": "Welcome to the BookStore!"}
+#     return JsonResponse(content)
